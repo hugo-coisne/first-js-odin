9d9c7e25d57835344c07d94dc7f8454cfc9f445c
awadesu <133776560+awadesu@users.noreply.github.com>
2023-05-22 12:44:27 +0200

    Add playRound function

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-function getComputerChoice(){
-    const choices = Array("Rock","Paper","Scissors");
-    const i = parseInt(Math.random()*10)%3;
+function getComputerChoice() {
+    const choices = Array("Rock", "Paper", "Scissors");
+    const i = parseInt(Math.random() * 100) % 3;
     return choices[i]
+}
+
+function playRound(playerSelection, computerSelection) {
+
+    const p = playerSelection.toLowerCase();
+    const player = p[0].toUpperCase() + p.slice(1);
+
+    const c = computerSelection.toLowerCase();
+    const computer = c[0].toUpperCase() + c.slice(1);
+
+    const win = (player == "Rock" && computer == "Scissors") || (player == "Paper" && computer == "Rock") || (player == "Scissors" && computer == "Paper");
+    const draw = player == computer;
+
+    if (win) {
+        return ("You win! " + player + " beats " + computer + "!");
+    } else if (draw) {
+        return ("Draw! " + player + " against " + computer + "!");
+    } else {
+        return ("You lose! " + computer + " beats " + player + "!");
+    }
+
 }
