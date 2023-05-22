116b91f6481d3ff4123432e9107f339fc9f893b3
awadesu <133776560+awadesu@users.noreply.github.com>
2023-05-22 13:36:38 +0200

    set game function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var score = 0;
+
 function getComputerChoice() {
     const choices = Array("Rock", "Paper", "Scissors");
     const i = parseInt(Math.random() * 100) % 3;
@@ -16,11 +18,29 @@ function playRound(playerSelection, computerSelection) {
     const draw = player == computer;
 
     if (win) {
+        score += 1;
         return ("You win! " + player + " beats " + computer + "!");
     } else if (draw) {
         return ("Draw! " + player + " against " + computer + "!");
     } else {
+        score -= 1;
         return ("You lose! " + computer + " beats " + player + "!");
     }
 
 }
+
+function game() {
+    for (var j = 0; j < 5; j++) {
+        const play = prompt("Please enter your choice : ");
+        console.log(playRound(play, getComputerChoice()));
+    }
+    if (score == 0) {
+        console.log("Draw!");
+    } else if (score < 0) {
+        console.log("You lost!");
+    } else {
+        console.log("You won!");
+    }
+}
+
+game();
